ff22e9de4a8062df3471d2023927b86f4e84b46d
Brian Rhee <45299876+brianbyrhee@users.noreply.github.com>
2022-02-09 23:24:01 -0500

    Update BankCLI.py

--- BankCLI.py
+++ BankCLI.py
@@ -1,12 +1,13 @@
-import decimal
-import math
+from decimal import Decimal
+from datetime import datetime
+import pickle
 
 class Bank:
     def __init__(self):
         self.accounts = None
 
     def createAccount(self, accountID, accountType, deposit):
-        newAccount = Account(accountID, accountType, deposit)
+        newAccount = CheckingAccount(accountID, accountType, deposit) if accountType == "checking" else SavingsAccount(accountID, accountType, deposit)
         if not self.accounts:
             self.accounts = [newAccount]
         else:
@@ -15,51 +16,59 @@ class Bank:
 class Account:
     def __init__(self, accountID, accountType, deposit):
         self.accountID = accountID
-        self.accountType = accountType
-        # deposit is a string
-        decimal.getcontext().prec = 2
-        decimal.getcontext().rounding = decimal.ROUND_HALF_UP
-        self.deposit = decimal.Decimal(deposit)
+        self.accountType = accountType.capitalize()
+        self.deposit = 0
         self.transactions = None
+        self.trueTransactionCounter = 0
+        self.addTransaction(Decimal(deposit), datetime.now(), True)
 
     def printTransactions(self):
-        for t in self.transactions:
-            print(t.date + ", " + t.amount)
+        sortedTransactions = sorted(self.transactions, key= lambda x: x.date)
+        for t in sortedTransactions:
+            print(t.date + ", $" + "{:,.2f}".format(t.amount))
 
-    def addTransaction(self, amount, date):
-        newTransaction = Transaction(amount, date)
+    def addTransaction(self, amount, date, trueTransaction):
+        newTransaction = Transaction(amount, date, trueTransaction)
         self.deposit += amount
+
         if not self.transactions:
             self.transactions = [newTransaction]
         else:
             self.transactions.append(newTransaction)
 
 class Transaction:
-    def __init__(self, amount, date):
+    def __init__(self, amount, date, trueTransaction):
         self.amount = amount
-        self.date = date
+        self.date = date if type(date) == str else date.strftime("%Y") + "-" +  date.strftime("%m") + "-" +  date.strftime("%d")
+        self.trueTransaction = trueTransaction
 
 class CheckingAccount(Account):
-    def __init__(self):
+    def calculateInterest(self):
         pass
 
+
 class SavingsAccount(Account):
-    def __init__(self):
+    def checkTransactionCountLimit(self):
+        pass
+
+    def calculateInterest(self):
         pass
 
 def parseInfo(acc):
-    zeroPad = 9 - acc.accountID
+    zeroPad = 9 - len(str(acc.accountID))
     #depositStr = "%.2f" % acc.deposit
-    depositStr = str(acc.deposit)
-    return acc.accountType + "#" + "0" * zeroPad + str(acc.accountID) + ",\tbalance: " + depositStr
+    depositStr = "{:,.2f}".format(acc.deposit)
+    return acc.accountType + "#" + "0" * zeroPad + str(acc.accountID) + ",\tbalance: $" + depositStr
 
 def main():
     accountID = 1
     bank = Bank()
+    currAccount = "None"
     while True:
-        currAccount = "None"
         # doublecheck on what exactly needs to be printed below
-        print("Currently selected account: " + currAccount)
+        accountPrint = currAccount if currAccount == "None" else parseInfo(currAccount)
+        print("--------------------------------")
+        print("Currently selected account: " + accountPrint)
         print("Enter command")
         print("1: open account")
         print("2: summary")
@@ -91,10 +100,8 @@ def main():
         elif task == "3":
             # 3: select account
             print("Enter account number")
-            print(len(bank.accounts))
             accNumber = input(">")
-            currAccount = parseInfo(bank.accounts[int(accNumber)-1])
-            
+            currAccount = bank.accounts[int(accNumber)-1]
 
         elif task == "4":
             # 4: list transactions
@@ -109,27 +116,45 @@ def main():
             year, month, day = date.split("-")
 
             # check if we're exceeding the trans limit
-            if currAccount.deposit + amount < 0:
+            if currAccount.deposit + Decimal(amount) < 0:
                 continue
             if type(currAccount) is SavingsAccount:
-                recentTransactionDates = [c.split("-") for c in currAccount.transactions[-2:]]
-                recentTransactionMonths = [c.split("-") for c in currAccount.transactions[-5:]]
-                if all(d == day for _,_,d in recentTransactionDates) or all(m == month for _,m,_ in recentTransactionMonths):
+                recentTransactionDates = []
+                for i in range(len(currAccount.transactions)-1,-1,-1):
+                    if currAccount.transactions[i].trueTransaction:
+                        recentTransactionDates.append(currAccount.transactions[i].date.split("-"))
+                    if len(recentTransactionDates) == 2:
+                        break
+
+                recentTransactionMonths = []
+                for i in range(len(currAccount.transactions)-1,-1,-1):
+                    if currAccount.transactions[i].trueTransaction:
+                        recentTransactionMonths.append(currAccount.transactions[i].date.split("-"))
+                    if len(recentTransactionMonths) == 5:
+                        break
+
+                if (len(recentTransactionDates) == 2 and all(d == day for _,_,d in recentTransactionDates)) or (len(recentTransactionMonths) == 5 and all(m == month for _,m,_ in recentTransactionMonths)):
                     continue
             
-            currAccount.addTransaction(amount, date)
+                
+            currAccount.addTransaction(Decimal(amount), date, True)
 
         elif task == "6":
             # 6: interest and fees
-            pass
+            if type(currAccount) is SavingsAccount:
+                currAccount.addTransaction(currAccount.deposit*Decimal(0.025), datetime.now(), False)
+            elif type(currAccount) is CheckingAccount:
+                currAccount.addTransaction(currAccount.deposit*Decimal(0.0015), datetime.now(), False)
+                if currAccount.deposit < 100:
+                    currAccount.addTransaction(Decimal(-10), datetime.now(), False)
 
         elif task == "7":
             # 7: save
-            pass
+            pickle.dump(bank, open("save.p", "wb"))
 
         elif task == "8":
             # 8: load
-            pass
+            pickle.load(open("save.p", "rb"))
 
         elif task == "9":
             # 9: quit
